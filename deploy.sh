e9ecede61844c80a43d8d5237ac11a140f60fb9a
syfun <sunyu418@gmail.com>
2017-03-08 19:52:13 +0800

    update CNAME

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ hugo
 
 echo "Updating gh-pages branch"
 cd public
-echo "blog.titangroupco.com" > CNAME
+echo "blog.syfun.net" > CNAME
 git add --all && git commit -m "Publishing to gh-pages (deploy.sh)"
 cd ..
 git push origin gh-pages
